92795f682816d434cd488bf4f822778a6eb2fac4
kumar <santanupaikaray23@gmail.com>
2021-06-20 13:54:02 +0530

    add all changes

--- nodeinbuilt/fs.js
+++ nodeinbuilt/fs.js
@@ -4,9 +4,16 @@ var fs = require('fs')
 //     if(err) throw err;
 //     console.log("File Appended")
 // })
-fs.readFile('db.json','utf-8',function(err,data){
-    if(err)throw err;
-    console.log(data)
-})
+// fs.readFile('db.json','utf-8',function(err,data){
+//     if(err)throw err;
+//     console.log(data)
+// })
  
-
+// fs.rename('MyCode.text','First.txt',function(err){
+//     if(err)throw err;
+//     console.log('File renamed')
+// })
+fs.unlink("First.txt",function(err){
+    if(err)throw err;
+    console.log('File Deleted')
+})
